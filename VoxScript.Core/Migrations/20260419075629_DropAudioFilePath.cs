fc91b08fc2ff732e97d14ad68d307cce51136bd9
Rohil Shah <rohilrshah@gmail.com>
2026-04-19 00:57:03 -0700

    feat: EF migration DropAudioFilePath

---
+++ VoxScript.Core/Migrations/20260419075629_DropAudioFilePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoxScript.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropAudioFilePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioFilePath",
+                table: "Transcriptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AudioFilePath",
+                table: "Transcriptions",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
